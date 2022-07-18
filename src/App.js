604bdb4c1d1768e97e44e525b19401437cad26b2
ndalilavin <vinson.steve@student.moringaschool.com>
2022-07-18 20:35:28 +0300

    Not done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from './logo.svg';
+import logo from './logo2.png';
 import './App.css';
 
 /**
@@ -12,43 +12,48 @@ const users = [
   { name: "Billy Doe", id: 3 }
 ];
 
+const userNames = users.map((user) => {
+  return <li key={user.id}>{user.name}</li>;
+});
+
+
 // comment this out after completion and uncomment code below it to proceed
 function Child() {
   return <div>This is children content</div>;
 }
-/**
-  Challenge: Uncomment this code to complete quiz
+// /**
+//   Challenge: Uncomment this code to complete quiz
 
-function Child() {
-  return (
-    <>
-      <div>Child</div>
-      <button>Change Parent Value</button>
-    </>
-  );
-}
+// function Child() {
+//   return (
+//     <>
+//       <div>Child</div>
+//       <button>Change Parent Value</button>
+//     </>
+//   );
+// }
 
-function Parent() {
-  const [value, setValue] = React.useState(
-    "I need to be updated from my child"
-  );
+// function Parent() {
+//   const [value, setValue] = React.useState(
+//     "I need to be updated from my child"
+//   );
 
-  return (
-    <>
-      <h3>Update Parent State Challenge (Using Callback)</h3>
-      <div className="wrapper">
-        <div>Parent</div>
-        <div className="box-wrapper">{value}</div>
-      </div>
+//   return (
+//     <>
+//       <h3>Update Parent State Challenge (Using Callback)</h3>
+//       <div className="wrapper">
+//         <div>Parent</div>
+//         <div className="box-wrapper">{value}</div>
+//       </div>
 
-      <div className="wrapper">
-        <Child />
-      </div>
-    </>
-  );
-}
-Uncomment this to tackle quiz
-**/
+//       <div className="wrapper">
+//         <Child />
+//       </div>
+//     </>
+//   );
+// }
+// Uncomment this to tackle quiz
+// **/
 
 // Comment out after completion
 function Parent() {
@@ -60,15 +65,20 @@ function Parent() {
 }
 // Comment above code after completion
 
+function DisplayJSXText(){
+  return <h2>JSX is Cool!</h2>
+}
+
+
 function App() {
-  const [] = React.useState(true);
-  return (
-    
-    <>
-    
+  const [show, setShow] = React.useState(true);
+  
+  return (  
+    <>   
     <img src={logo} className="App-logo" alt="logo" />
+      <DisplayJSXText/>
       <h3>User names</h3>
-      <ul></ul>
+      <ul>{userNames}</ul>
       <button>Hide Element Below</button>
 
       <div>Toggle Challenge</div>
